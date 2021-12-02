9768dfeb29b3f19cf45b986bf2b86480527f9b2a
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 21:56:39 +0300

    define and anotate variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+variables defined and anotated with
+a given value
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
