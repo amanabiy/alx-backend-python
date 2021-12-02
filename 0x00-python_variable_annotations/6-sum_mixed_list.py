cdc8624279cfa957d4216c8a29bfb08c7329c02b
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 22:16:49 +0300

    union elements

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function sum_mixed_list 
+which takes a list mxd_lst of integers
+and floats and  returns their sum as a float.
+"""
+from typing import List, Union
+
+def sum_mixed_list(nums: List[Union[int, float]]) -> float:
+    """
+    accepts both integers and floats in the list and
+    returns the sum of the elements as float
+    """
+    return sum(nums)
