e949e060184a745652a88042426c863edeffd0c8
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 21:51:55 +0300

    float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function to_str that takes
+a float n as argument and returns the string
+representation of the float
+"""
+
+
+def to_str(num: float) -> str:
+    """
+    returns the string representation of the float number
+    """
+    return "{}".format(num)
