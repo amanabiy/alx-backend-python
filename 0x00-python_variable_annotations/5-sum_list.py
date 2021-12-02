eda81fb0639cfe1e89450ea1bb1ef147e9dd2901
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 22:02:19 +0300

    sum all the number in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ a type-annotated function sum_list which takes
+ a list input_list of floats as argument and
+ returns their sum as a float.
+"""
+
+
+def sum_list(nums: list[int]) -> float:
+    """ adds all the numbers in the list and
+    returns a float number"""
+    return sum(nums)
