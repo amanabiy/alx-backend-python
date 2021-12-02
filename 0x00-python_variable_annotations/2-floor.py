a136c6c11b344e6cb027df2622b9b9739be97c28
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 21:47:32 +0300

    floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+ a type-annotated function floor which takes
+ a float n as argument and
+ returns the floor of the float.
+"""
+
+
+def floor(num: float) -> int:
+    """ takes in float number and
+    floor it to nearest int value
+    """
+    return int(num)
