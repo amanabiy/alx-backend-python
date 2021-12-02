ebd7872204f2d5cb96588b2ea91a8b47964d8129
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 22:05:57 +0300

    use the typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import List
 """
  a type-annotated function sum_list which takes
  a list input_list of floats as argument and
@@ -6,7 +7,7 @@
 """
 
 
-def sum_list(nums: list[float]) -> float:
+def sum_list(nums: List[float]) -> float:
     """ adds all the numbers in the list and
     returns a float number"""
     return sum(nums)
