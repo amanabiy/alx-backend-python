072c8679ab78f0b2cd2281949bfe86d0881ea70e
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 22:17:46 +0300

    fixed lint error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-a type-annotated function sum_mixed_list 
+a type-annotated function sum_mixed_list
 which takes a list mxd_lst of integers
 and floats and  returns their sum as a float.
 """
 from typing import List, Union
+
 
 def sum_mixed_list(nums: List[Union[int, float]]) -> float:
     """
