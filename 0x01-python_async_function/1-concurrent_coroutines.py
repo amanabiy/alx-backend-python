05b7bea16943e0982ab3fb0e9ebc78ba11cf5215
amanabiy <amanuelsisay.as@gmail.com>
2021-12-06 22:10:10 +0300

    n number of times

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+runs wait_random n times with max_delay as max
+return the list of all the delays (float values).
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay) -> List[float]:
+    """ runs wait_random n times with max_delay as max """
+    all_delays: List = []
+    for i in range(n):
+        num: int = await wait_random(max_delay)
+        all_delays.append(num)
+    return all_delays
