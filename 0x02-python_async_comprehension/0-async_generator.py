6a6cacbb8e8f4b14d910cd8b6d157fa85a8b11dd
amanabiy <amanuelsisay.as@gmail.com>
2021-12-07 21:42:58 +0300

    add typing and await the sleep time

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,12 @@ The coroutine will loop 10 times, each time asynchronously wait 1 second,
 then yield a random number between 0 and 10. Use the random module.
 """
 import random
-from typing import List
+from typing import Generator
 import asyncio
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ loops ten times and yield a random number between 0 and 10"""
     for i in range(10):
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
         yield random.random() * 10
